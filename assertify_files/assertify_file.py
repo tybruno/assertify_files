ed199d8f91df1f5444b943d92e2c26c94ea9b165
tybruno <tybruno@cisco.com>
2021-08-05 11:49:58 -0700

    refactored assertify_file.py

--- assertify_files/assertify_file.py
+++ assertify_files/assertify_file.py
@@ -3,81 +3,53 @@ from dataclasses import (
     field,
 )
 from pathlib import Path
-from string import Template
 from typing import (
     Callable,
-    Iterable,
-    Optional,
-    Type,
-    Union,
 )
 
-from assertifiers.container import AssertifyIn
-from assertifiers.logic import (
-    AssertifyFalse,
-    AssertifyTrue,
+from assertify_files.assertify_path import (
+    AssertifyPathFileExists,
+    AssertifyPathFileExtension,
+    AssertifyPathFileNotExists,
 )
 
-from assertify_files.base import AbstractAssertiyFile
-from assertify_files.str_to_path import StrToPath
+
+@dataclass
+class StrToPath:
+    def __call__(self, string: str) -> Path:
+        if isinstance(string, str):
+            path = Path(string)
+            return path
+        elif isinstance(string, Path):
+            return string
+
+        raise TypeError(
+            f"{string} is of type {type(string)} not {type(str())}"
+        )
 
 
 @dataclass
-class AssertifyFileExtension(AbstractAssertiyFile):
-    valid_extensions: Iterable
+class AssertifyFileExtension(AssertifyPathFileExtension):
     convert_to_path: Callable = field(default=StrToPath(), init=False)
-    raises: Optional[
-        Union[None, Type[Exception], Type[AssertionError]]
-    ] = field(default=ValueError)
-    msg: Optional[Union[None, str, Template]] = field(
-        default=Template(
-            "'$file' does not have a valid file extension $valid_extensions"
-        )
-    )
 
-    def __call__(self, file: Union[str, Path]) -> bool:
+    def __call__(self, file: str) -> bool:
         path = self.convert_to_path(file)
-
-        asserity_in = AssertifyIn(
-            raises=self.raises,
-            msg=self.msg.substitute(
-                file=file, valid_extensions=self.valid_extensions
-            ),
-        )
-        return asserity_in(member=path.suffix, container=self.valid_extensions)
+        return super().__call__(path=path)
 
 
 @dataclass
-class AssertifyFileExists(AbstractAssertiyFile):
+class AssertifyFileExists(AssertifyPathFileExists):
     convert_to_path: Callable = field(default=StrToPath(), init=False)
-    raises: Optional[
-        Union[None, Type[Exception], Type[AssertionError]]
-    ] = field(default=FileNotFoundError)
-    msg: Optional[Union[None, str, Template]] = field(
-        default=Template("'$file' is not a file")
-    )
 
-    def __call__(self, file: Union[str, Path]) -> bool:
+    def __call__(self, file: str) -> bool:
         path = self.convert_to_path(file)
-        assertify_true = AssertifyTrue(
-            raises=self.raises, msg=self.msg.substitute(file=file)
-        )
-        return assertify_true(expr=path.is_file())
+        return super().__call__(path=path)
 
 
 @dataclass
-class AssertifyFileNotExists(AbstractAssertiyFile):
+class AssertifyFileExists(AssertifyPathFileNotExists):
     convert_to_path: Callable = field(default=StrToPath(), init=False)
-    raises: Optional[
-        Union[None, Type[Exception], Type[AssertionError]]
-    ] = field(default=FileExistsError)
-    msg: Optional[Union[None, str, Template]] = field(
-        default=Template("'$file' exists")
-    )
 
-    def __call__(self, file: Union[str, Path]) -> bool:
+    def __call__(self, file: str) -> bool:
         path = self.convert_to_path(file)
-        assertify_false = AssertifyFalse(
-            raises=self.raises, msg=self.msg.substitute(file=file)
-        )
-        return assertify_false(expr=path.is_file())
+        return super().__call__(path=path)
